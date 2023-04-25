28b6ea839295ebdd7f71f879957e6772c77fbeb8
Jay <jayarosorno@gmail.com>
2023-04-25 13:49:36 +0800

    some changes in css and scripts

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import "./App.css";
 
 function App() {
+  const menu = [
+    {
+      id: 1,
+      icon: <i className="fa fa-icons"></i>,
+      p: "Lorem ipsum",
+    },
+    {
+      id: 2,
+      icon: <i className="fa fa-icons"></i>,
+      p: "second 2",
+    },
+    {
+      id: 3,
+      icon: <i className="fa fa-icons"></i>,
+      p: "third",
+    },
+    {
+      id: 4,
+      icon: <i className="fa fa-icons"></i>,
+      p: "forth forth",
+    },
+    {
+      id: 5,
+      icon: <i className="fa fa-icons"></i>,
+      p: "fifth lima",
+    },
+  ];
   return (
     <div className="App">
       <nav>
         <h1>LOGO</h1>
         <ul>
           <p>Menu</p>
-          <li>
-            <i class="fa fa-icons"></i> Lorem ipsum
-          </li>
-          <li>
-            <i class="fa fa-icons"></i> Lorem ipsum
-          </li>
-          <li>
-            <i class="fa fa-icons"></i> Lorem ipsum
-          </li>
-          <li>
-            <i class="fa fa-icons"></i> Lorem ipsum
-          </li>
+          {menu.map((link) => (
+            <li key={link.id}>
+              {link.icon} {link.p}
+            </li>
+          ))}
         </ul>
         <ul>
           <p>Forms & Tables</p>
           <li>
-            <i class="fa fa-icons"></i> Lorem ipsum
+            <i className="fa fa-icons"></i> Lorem ipsum
           </li>
           <li>
-            <i class="fa fa-icons"></i> Lorem ipsum
+            <i className="fa fa-icons"></i> Lorem ipsum
           </li>
           <li>
-            <i class="fa fa-icons"></i> Lorem ipsum
+            <i className="fa fa-icons"></i> Lorem ipsum
           </li>
         </ul>
         <div>
@@ -40,25 +60,25 @@ function App() {
         <h2>Profile Statistics</h2>
         <div className="main-1div">
           <div>
-            <i class="fa fa-icons"></i>
+            <i className="fa fa-icons"></i>
             <p>
               Profile views <br /> 123,156
             </p>
           </div>
           <div>
-            <i class="fa fa-icons"></i>
+            <i className="fa fa-icons"></i>
             <p>
               Profile views <br /> 123,156
             </p>
           </div>
           <div>
-            <i class="fa fa-icons"></i>
+            <i className="fa fa-icons"></i>
             <p>
               Profile views <br /> 123,156
             </p>
           </div>
           <div>
-            <i class="fa fa-icons"></i>
+            <i className="fa fa-icons"></i>
             <p>
               Profile views <br /> 123,156
             </p>
